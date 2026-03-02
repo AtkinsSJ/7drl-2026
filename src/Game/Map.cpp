4fdae3cd9d2d40c192a9d0ffc7f3528e18987f77
Sam Atkins <atkinssj@gmail.com>
2026-03-02 16:31:26 +0000

    Only draw the area around the player
    Turns out that drawing all 1024*1024 tiles every frame was a bit
    expensive, even on my beast of a machine. Probably got work to do to
    speed that up.

--- src/Game/Map.cpp
+++ src/Game/Map.cpp
@@ -45,17 +45,32 @@ void Map::render(float delta_time) const
 {
     auto& renderer = the_renderer();
 
+    // Calculate the tile area that's visible to the player.
+    // We err on the side of drawing too much, rather than risking having holes in the world.
+    auto& world_camera = renderer.world_camera();
+    Rect2I visible_tile_bounds = Rect2I::create_centre_size(
+        v2i(world_camera.position()), v2i(world_camera.size() / world_camera.zoom()) + v2i(3, 3));
+    visible_tile_bounds = visible_tile_bounds.intersected({ 0, 0, static_cast<s32>(width()), static_cast<s32>(height()) });
+
     // Draw tiles
-    for (auto y = 0; y < m_tiles.h; ++y) {
-        for (auto x = 0; x < m_tiles.w; ++x) {
+    for (s32 y = visible_tile_bounds.y();
+        y < visible_tile_bounds.y() + visible_tile_bounds.height();
+        y++) {
+        for (s32 x = visible_tile_bounds.x();
+            x < visible_tile_bounds.x() + visible_tile_bounds.width();
+            x++) {
             auto& terrain_sprite = tile_at(x, y).terrain_sprite();
             drawSingleSprite(&renderer.world_buffer(), &terrain_sprite, { x, y, 1, 1 }, renderer.shaderIds.pixelArt, Colour::white());
         }
     }
 
     // Draw actors
-    for (auto y = 0; y < m_tiles.h; ++y) {
-        for (auto x = 0; x < m_tiles.w; ++x) {
+    for (s32 y = visible_tile_bounds.y();
+        y < visible_tile_bounds.y() + visible_tile_bounds.height();
+        y++) {
+        for (s32 x = visible_tile_bounds.x();
+            x < visible_tile_bounds.x() + visible_tile_bounds.width();
+            x++) {
             if (auto* actor = tile_at(x, y).actor())
                 actor->render(delta_time);
         }
